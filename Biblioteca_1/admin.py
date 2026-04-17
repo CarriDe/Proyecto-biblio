a342752038d33f172ccd177395044eb0c2ace35d
CarriDe <carrillodanielr@gmail.com>
2026-04-17 21:58:33 +0200

    Adaptamos admin.py con los roles

--- Biblioteca_1/admin.py
+++ Biblioteca_1/admin.py
@@ -2,19 +2,20 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 
-from .models import PerfilUsuario
-
-
-class PerfilUsuarioInline(admin.StackedInline):
-    model = PerfilUsuario
-    can_delete = False
-    verbose_name_plural = 'Perfil de usuario'
+from .models import Libro, Usuario
 
 
 class UsuarioAdmin(BaseUserAdmin):
-    inlines = (PerfilUsuarioInline,)
+    list_display = ('username', 'email', 'first_name', 'last_name', 'rol', 'is_staff')
+    list_filter = ('rol', 'is_staff', 'is_superuser', 'is_active')
+    fieldsets = BaseUserAdmin.fieldsets + (
+        ('Información adicional', {'fields': ('rol',)}),
+    )
+    add_fieldsets = BaseUserAdmin.add_fieldsets + (
+        ('Información adicional', {'fields': ('rol',)}),
+    )
 
 
 admin.site.unregister(User)
-admin.site.register(User, UsuarioAdmin)
-admin.site.register(PerfilUsuario)
+admin.site.register(Usuario, UsuarioAdmin)
+admin.site.register(Libro)

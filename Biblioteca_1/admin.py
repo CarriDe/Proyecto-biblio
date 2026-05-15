92b552c7184898afd92f1c41a524007132fc81ee
CarriDe <carrillodanielr@gmail.com>
2026-05-15 20:39:54 +0200

    Registramos modelos actuales en admin

--- Biblioteca_1/admin.py
+++ Biblioteca_1/admin.py
@@ -3,7 +3,7 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 from django.contrib.admin.sites import NotRegistered
 
-from .models import Libro, Usuario
+from .models import Autor, Categoria, Libro, Prestamo, Usuario
 
 
 class UsuarioAdmin(BaseUserAdmin):
@@ -17,9 +17,26 @@ class UsuarioAdmin(BaseUserAdmin):
     )
 
 
+class LibroAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'tipo', 'disponible', 'validado', 'prestado', 'categoria', 'administrador_validar')
+    list_filter = ('tipo', 'disponible', 'validado', 'prestado', 'categoria')
+    search_fields = ('titulo', 'descripcion')
+    filter_horizontal = ('autores', 'usuarios_prestamo')
+
+
+class PrestamoAdmin(admin.ModelAdmin):
+    list_display = ('libro', 'usuario', 'fecha_prestamo', 'fecha_devolucion', 'devuelto')
+    list_filter = ('devuelto',)
+    search_fields = ('libro__titulo', 'usuario__username')
+
+
 try:
     admin.site.unregister(User)
 except NotRegistered:
     pass
+
 admin.site.register(Usuario, UsuarioAdmin)
-admin.site.register(Libro)
+admin.site.register(Autor)
+admin.site.register(Categoria)
+admin.site.register(Libro, LibroAdmin)
+admin.site.register(Prestamo, PrestamoAdmin)

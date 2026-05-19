7536781fde7e6227f8fa9738658b8a460c684c6f
CarriDe <carrillodanielr@gmail.com>
2026-05-19 19:13:58 +0200

    cambios admin 3, custom indice

--- config/urls.py
+++ config/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from Biblioteca_1.admin import custom_admin_site
+
 urlpatterns = [
     path('', include('Biblioteca_1.urls')),
-    path('admin/', admin.site.urls),
+    path('admin/', custom_admin_site.urls),
 ]
